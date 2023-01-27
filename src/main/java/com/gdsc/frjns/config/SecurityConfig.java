73f2ddf750187c5353739efef7b6f93197b122cf
HanHyunSoo <gustn4563@gmail.com>
2023-01-27 19:30:58 +0900

    fix: Edit build.gradle and uri permit

--- src/main/java/com/gdsc/frjns/config/SecurityConfig.java
+++ src/main/java/com/gdsc/frjns/config/SecurityConfig.java
@@ -6,10 +6,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.CorsUtils;
@@ -37,7 +34,7 @@ public class SecurityConfig {
                 .and()
                 .authorizeHttpRequests()
                     .requestMatchers(CorsUtils::isPreFlightRequest).permitAll()
-                    .requestMatchers("/api/*").permitAll()
+                    .requestMatchers("/**").permitAll()
                     .anyRequest().authenticated()
                 .and()
                 .build();
