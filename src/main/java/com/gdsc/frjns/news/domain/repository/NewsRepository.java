96e8dd04daa7c6dc46a81ad9b8d2a13a9b5960bf
HanHyunSoo <gustn4563@gmail.com>
2023-01-27 23:34:24 +0900

    refactor: Refactor Code

--- src/main/java/com/gdsc/frjns/news/domain/repository/NewsRepository.java
+++ src/main/java/com/gdsc/frjns/news/domain/repository/NewsRepository.java
@@ -1,14 +1,10 @@
-package com.gdsc.frjns.news.repository;
+package com.gdsc.frjns.news.domain.repository;
 
-import com.gdsc.frjns.news.domain.News;
+import com.gdsc.frjns.news.domain.model.News;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Slice;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.List;
-
 public interface NewsRepository extends JpaRepository<News, Long> {
-
-    List<News> findAll();
     Slice<News> findAllBy(Pageable pageable);
 }
